0375f4177c0b29034737ff73da4372fb62841a02
Yamachan-54 <gojuuyonnki@gmail.com>
2025-06-14 12:19:17 +0900

    完成

---
+++ example.py
@@ -0,0 +1,33 @@
+print("hello world")
+
+
+def greet():
+	print("こんにちは")
+
+
+greet()
+
+
+def print_name(name):
+	print(f"私の名前は{name}です")
+
+name = "やまちゃん"
+
+print_name(name)
+
+
+
+def get_greet():
+	return "おはようございます"
+
+print(get_greet())
+
+
+
+def add(a, b):
+	return a + b
+
+a = 10 
+b = 20
+
+print(add(a, b))
